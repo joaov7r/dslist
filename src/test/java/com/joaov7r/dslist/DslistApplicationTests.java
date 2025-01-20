b12918449bdea8d1bcc5a1b3c4c30aed06474304
joaov7r <jvc.lopes@outlook.com>
2025-01-20 18:03:34 -0300

    Project created

---
+++ src/test/java/com/joaov7r/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaov7r.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
